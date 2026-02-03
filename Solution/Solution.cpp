a9425aaea881699df419292f76c313808f07d8f0
Matin Afzal <contact.matin@yahoo.com>
2026-02-04 01:48:19 +0330

    F this country im losing my mind!

--- Solution/Solution.cpp
+++ Solution/Solution.cpp
@@ -2,6 +2,26 @@
 #include <iostream>
 
 typedef long Fixed32; // 16.16 Fixed Point 
+Fixed32 FixedMul(Fixed32 x, Fixed32 y);
+Fixed32 FixedDiv(Fixed32 x, Fixed32 divisor);
+
+#pragma aux FixedMul =          \
+    "imul edx"                  \
+    "add eax, 8000h"            \
+    "adc edx, 0"                \
+    "shrd eax, edx, 16"         \
+    parm caller [eax] [edx      \
+    value [eax]                 \
+    modify [eax edx];
+
+#pragma aux FixedDiv =          \
+    "xor eax, eax"              \
+    "shrd eax, edx, 16"         \
+    "sar edx, 16"               \
+    "idiv ebx"                  \
+    parm caller [edx] [ebx]     \
+    value [eax]                 \
+    modify [eax ebx edx];
 
 //*********************************************************
 #define INT_TO_FIXED(x)     ((x) << 16)
@@ -13,6 +33,8 @@ typedef long Fixed32; // 16.16 Fixed Point
 #define FixedAdd(x, y)  (x + y)
 #define FixedSub(x, y)  (x - y)
 //*********************************************************
+#define PRINT_INFO(info)    std::cout<<"\n"<<info<<std::endl;
+//*********************************************************
 #define ONE INT_TO_FIXED(1);
 #define FIXED_PI        205887L
 #define FIXED_2PI       411775L
@@ -22,9 +44,23 @@ typedef long Fixed32; // 16.16 Fixed Point
 #define FIXED_GOLDEN    106039L
 //*********************************************************
 
+void testAdd();
+
 int main()
 {
     std::cout << "Fixed32 (long) is " << sizeof(Fixed32) << " bytes." << std::endl;
+    testAdd();
+
+    PRINT_INFO("Enter any keys to exit.");
     std::cin.get();
     return EXIT_SUCCESS;
+}
+//---------------------------------------------------------
+void testAdd()
+{
+    PRINT_INFO("*********Starting testAdd!*********");
+    Fixed32 x = 8000.8045;
+    Fixed32 y = 0999.0954;
+    Fixed32 res = FixedAdd(x, y);
+    std::cout << "Fixed32 x + y = " << res << " eq=al to double " << FIXED_TO_DOUBLE(res) << std::endl;
 }
